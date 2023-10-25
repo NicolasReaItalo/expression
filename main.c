6e10f77dabd9bd3b880adee8daf7110bde4cbddc
NicolasReaItalo <nicolas.rea@gmail.com>
2023-10-25 21:52:31 +0200

    initial main

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "expression.h"
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		printf("Usage : expression <mathematical expression to evaluate>\n");
+		return (0);
+	}
+	return (0);
+}
